233b13c1198f6294181cab17ec21b654290e9da9
Dragos <emailul-tau-de-github@example.com>
2026-06-20 20:11:24 +0300

    perf: optimize fonts with next/font, add preconnect to APIs - faster LCP and no render blocking

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Space_Grotesk, Space_Mono } from 'next/font/google';
 import BackToTop from '../components/BackToTop';
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-grotesk',
+  display: 'swap',
+});
+
+const spaceMono = Space_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-mono-next',
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -10,8 +25,10 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
-      <BackToTop />
+      <main className={`${spaceGrotesk.variable} ${spaceMono.variable}`} style={{ display: 'contents' }}>
+        <Component {...pageProps} />
+        <BackToTop />
+      </main>
     </>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Space+Mono:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="preconnect" href="https://api.coingecko.com" />
+        <link rel="preconnect" href="https://googbmkxhsbjrscyfnzk.supabase.co" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
